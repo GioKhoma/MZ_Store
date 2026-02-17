038b03e0d4087946f26e0b2025e111da1c5543df
giokhomaa <giokhomaa@gmail.com>
2026-02-17 15:19:18 +0400

    Lecture 32 GenericAPIView & Mixins

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path("products/", views.products, name="products"),
     path("reviews/", views.review_view, name="review_view"),
+
+    path('product-apiview/', views.ProductView.as_view(), name='product'),
+    path('product-apiview/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
+
+    path('products-genericapiview/', views.ProductAPIView.as_view(), name="products_genericapiview"),
+    path('products-genericapiview/<int:pk>', views.ProductAPIView.as_view(), name='product_genericapiview'),
 ]
--- products/views.py
+++ products/views.py
@@ -89,3 +89,104 @@ def review_view(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+
+
+class ProductView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+class ProductDetail(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            product = serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+
+
+
+
+
+
+
+
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import (
+    RetrieveModelMixin,
+    CreateModelMixin,
+    ListModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+)
+
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductAPIView(RetrieveModelMixin,
+                    CreateModelMixin,
+                    ListModelMixin,
+                    UpdateModelMixin,
+                    DestroyModelMixin,
+                    GenericAPIView):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, pk=None, *args, **kwargs):
+        if pk:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+
